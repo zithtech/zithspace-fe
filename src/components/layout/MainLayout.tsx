b66632d32d717ebb2167ffba1b9a7a97d9166628
Dinesh Kumar <dkdineshkumar2001@gmail.com>
2026-01-30 00:44:01 +0530

    Payslip

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -279,6 +279,51 @@ export default function MainLayout({ children }: MainLayoutProps) {
       ],
 
     },
+
+    {
+      key: "salary",
+      icon: <MoneyCollectOutlined />,
+      label: "Payroll",
+      children: [
+        // {
+        //   key: "/salary/Dashboard",
+        //   icon: <DashboardOutlined />,
+        //   label: "Dashboard",
+        //   onClick: () => handleNavigation("/salary/Dashboard"),
+        // },
+       
+        {
+          key: "/salary/Create-payslip",
+          icon: <FileAddOutlined />,
+          label: "Create Payslip",
+          onClick: () => handleNavigation("/salary/Create-payslip"),
+        },
+        {
+          key: "/salary/My-Payslip",
+          icon: <ProfileOutlined />,
+          label: "My Payslip",
+          onClick: () => handleNavigation("/salary/My-payslip"),
+        },
+        {
+          key: "/salary/Generate-payslip",
+          icon: <FileAddOutlined />,
+          label: "Generate Payslip",
+          onClick: () => handleNavigation("/salary/Generate-payslip"),
+        },
+        {
+          key: "/salary/Payslips",
+          icon: <SnippetsOutlined />,
+          label: "Payslips",
+          onClick: () => handleNavigation("/salary/Payslips"),
+        },
+         {
+          key: "/salary/Settings",
+          icon: <SettingOutlined />,
+          label: "Settings",
+          onClick: () => handleNavigation("/salary/Settings"),
+        },
+      ],
+    },
     {
       key: "/documenthub",
       icon: <FileZipOutlined />,

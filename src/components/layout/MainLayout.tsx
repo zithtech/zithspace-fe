e15e4d431355f96e9a63243c767e7b82c44b32af
Dinesh Kumar <dkdineshkumar2001@gmail.com>
2026-03-03 00:17:52 +0530

    UI Fixes

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -109,16 +109,13 @@ export default function MainLayout({ children }: MainLayoutProps) {
         <Content
           style={{
             margin: 0,
-            //padding: 0,
-            //background: "#f5f5f5",
-            background:'white',
-            marginLeft: collapsed ? 60 : 240,
+            padding: 0,
+            background: "#f5f5f5",
+            marginLeft: collapsed ? 80 : 240,
             transition: "all 0.2s",
             height: "calc(100vh - 64px)",
             overflow: "auto",
             position: "relative",
-            paddingLeft:"24px",
-            paddingRight:"24px"
           }}
         >
           {children}
